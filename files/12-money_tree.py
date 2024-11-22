3afd9130a467db451572b3156be29291442be16f
mjmacarty <matt.macarty@gmail.com>
2024-11-22 18:49:18 -0500

    class files

--- files/12-money_tree.py
+++ files/12-money_tree.py
@@ -2,11 +2,11 @@
 starting_balance = 1000
 balance = starting_balance
 rate = 0.02
-withdrawal = 250
+withdrawal = 200
 goal = 1e6
 month = 0
 
-print(f"{'Month':<15}{'Balance':>10}")
+print(f"{'Month':<15}{'Balance':>12}")
 print("-" * 25)
 
 while balance < goal:
@@ -15,13 +15,13 @@ while balance < goal:
     if month % 12 == 0:
         balance -= withdrawal
         print()
-        print(f"{'Month':<15}{'Balance':>10}")
-        print("-" * 25)
-    print(f"{month:<15}{balance:10,.2f}")
+        print(f"{'Month':<15}{'Balance':>12}")
+        print("-" * 27)
+    print(f"{month:<15}{balance:12,.2f}")
 
 years = month / 12
 CAGR = (balance / starting_balance) ** (1/years) - 1
 
-print("-" * 25)
+print("-" * 27)
 print(f"It took {years:.1f} years")
 print(f"you earned: {CAGR:.2%}")
